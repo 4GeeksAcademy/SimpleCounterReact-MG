100057b699dc1812cb0c54ac3a69d215cd45704c
Manuel Garay <84086007+uykarma@users.noreply.github.com>
2023-11-06 05:43:52 +0000

    [Creacion y modificacion de archivos necesarios]
    - Se crea el archivo JS que contendra el contador
    - Se cambia el nombre y enlace del archivo home por buenas practicas

---
+++ src/js/component/SecondsCounter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+const SecondsCounter = () => {
+  return (
+    <div>
+       <FontAwesomeIcon icon={faClock} />
+    </div>
+  )
+}
+
+export default SecondsCounter
